7465cb20d80d70209db05bf5c1b0dd4e8f52ad77
Sagar Jauhari <sagarjauhari@gmail.com>
2013-12-01 14:34:56 -0500

    Added definition of sis_vpm_simulate()

--- vpm.py
+++ vpm.py
@@ -4,38 +4,26 @@ Created on Sun Nov 24 17:49:17 2013
 
 @author: sagar jauhari
 """
-from os.path import join
-from igraph import Graph
-import scipy.linalg
 
 try:
     from config import *
 except ImportError:
     raise ImportError("Config file unavailable")
 
-def file2igraph(file):
+def sis_vpm_simulate(graph, B, D, c, t):
     """
-    Converts graph file into iGraph object, adds artifacts
+    Simulates the propagation of a virus with the SIS VPM
+    graph: contact network
+    B: transmission probability
+    D: healing probability
+    c: number of initially infected nodes
+    t: number of time steps
+
+    The initially infected nodes should be chosen from a random uniform
+    probability distribution. At each time step, every susceptible (i.e., non-
+    infected) node has a β probability of being infected by neighboring
+    infected nodes, and every infected node has a δ probability of healing and
+    becoming susceptible again. The program also calculates the fraction
+    of infected nodes at each time step.
     """
-    with open(file, 'r') as fi:
-        v,e = fi.next().split()
-        e_list = [(int(i.split()[0]), int(i.split()[1])) for i in list(fi)]
-        assert (int(e) == len(e_list)),\
-                    "#edges mentioned and # of edges in file differ"
-        g = Graph()
-        g.add_vertices(int(v))
-        g.add_edges(e_list)
-        return g
-
-def get_eff_strength(graph, b,d):
-    M = len(list(graph.vs))
-    eig = scipy.linalg.eigh(graph.get_adjacency().data,
-                            eigvals_only=True,
-                            eigvals=(M-1, M-1))
-
-    print 'Largest Eigen Value: %0.3f'% (eig)
-    return eig*b/d
-
-graph = file2igraph(join(DATA_URL,'static.network'))
-eff_strength = get_eff_strength(graph, B1, D1)
-
+    pass
